13e9a99a9c7ea735951677c7afea3ea4e472bd6a
Shweta Ranjan Anand <72426535+Shweta200126@users.noreply.github.com>
2021-11-28 23:39:26 +0530

    Adding transparent bg theme
    - [X] Name: trans_bg

--- themes/index.js
+++ themes/index.js
@@ -342,6 +342,13 @@ const themes = {
     text_color: "bcbcbc",
     bg_color: "262626",
   },
+  trans_bg: {
+    title_color: "2f80ed",
+    icon_color: "4c71f2",
+    text_color: "333",
+    bg_color: "none",
+    border_color: "e4e2e2",
+  }
 };
 
 module.exports = themes;
