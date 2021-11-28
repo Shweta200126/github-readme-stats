86142cdb2ade2d22daf5d378cf2a90eee146ba02
Shweta Ranjan Anand <72426535+Shweta200126@users.noreply.github.com>
2021-11-28 23:45:59 +0530

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -346,7 +346,7 @@ const themes = {
     title_color: "2f80ed",
     icon_color: "4c71f2",
     text_color: "333",
-    bg_color: "none",
+    bg_color: "transparent",
     border_color: "e4e2e2",
   },
 };
