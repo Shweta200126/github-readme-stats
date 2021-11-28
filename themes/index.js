a8bdbba06d27e6b5bc71440d1c2204c7a9e87b74
Shweta Ranjan Anand <72426535+Shweta200126@users.noreply.github.com>
2021-11-28 23:55:49 +0530

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -3,7 +3,7 @@ const themes = {
     title_color: "2f80ed",
     icon_color: "4c71f2",
     text_color: "333",
-    bg_color: "000000",
+    bg_color: "ffffff",
     border_color: "e4e2e2",
   },
   default_repocard: {
